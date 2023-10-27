acbe71ac92d603fe723460d2893e703263580ed0
Krzychu <czernicki.krz@gmail.com>
2023-10-27 16:46:32 +0200

    feat(users/models): create User and CustomUserManager classes

--- EDMS/users/models.py
+++ EDMS/users/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, first_name, last_name, **extra_fields):
+        if not email:
+            raise ValueError("Email must be provided")
+        if not password:
+            raise ValueError("Password must be provided")
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+
+    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True, max_length=255)
+    # phone_number = models.CharField(max_length=50, null=True)
+    # birthday = models.DateField(null=True)
+    # # address = models.ForeignKey(Address, null=True)
+    # gross_salary = models.DecimalField(max_digits=6, decimal_places=2)
+    # holidays_days = models.PositiveSmallIntegerField(default=26)
+    # date_joined = models.DateField(auto_now_add=True)
+
+    # date_joined
+    # email_verified
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
