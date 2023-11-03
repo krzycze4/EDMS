7e41d8a12779192369590c1cfbec29a4d872f09d
Krzychu <czernicki.krz@gmail.com>
2023-11-03 13:48:01 +0100

    feat(users/urls): create end points 'register/', '', 'success-register' and 'activate/<str:uidb64>/<str:token>/'

--- EDMS/users/urls.py
+++ EDMS/users/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import (
+    ActivateAccountView,
+    CustomLoginView,
+    SuccessRegisterView,
+    UserRegisterView,
+)
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("", CustomLoginView.as_view(), name="login"),
+    path("success-register/", SuccessRegisterView.as_view(), name="success_register"),
+    path(
+        "activate/<str:uidb64>/<str:token>/",
+        ActivateAccountView.as_view(),
+        name="activate_account",
+    ),
 ]
