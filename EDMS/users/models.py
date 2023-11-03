7efef0555ee533b92b11719be1a18d564b44b2ee
Krzychu <czernicki.krz@gmail.com>
2023-11-03 13:28:25 +0100

    feat(users/models): create custom User model nad CustomUserManager

--- EDMS/users/models.py
+++ EDMS/users/models.py
@@ -1,32 +1,38 @@
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+import unicodedata
+
+from django.apps import apps
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):
-    def _create_user(self, email, password, first_name, last_name, **extra_fields):
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
         if not email:
-            raise ValueError("Email must be provided")
-        if not password:
-            raise ValueError("Password must be provided")
-
-        user = self.model(
-            first_name=first_name,
-            last_name=last_name,
-            email=self.normalize_email(email),
-            **extra_fields
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
         )
-
-        user.set_password(password)
+        email = GlobalUserModel.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, first_name, last_name, **extra_fields):
+    def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, first_name, last_name, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+    def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -35,27 +41,48 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, first_name, last_name, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
+
+    def get_by_natural_key(self, email):
+        return self.get(**{self.model.EMAIL_FIELD: email})
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(db_index=True, unique=True, max_length=255)
+class User(AbstractUser):
     # phone_number = models.CharField(max_length=50, null=True)
     # birthday = models.DateField(null=True)
     # # address = models.ForeignKey(Address, null=True)
     # gross_salary = models.DecimalField(max_digits=6, decimal_places=2)
     # holidays_days = models.PositiveSmallIntegerField(default=26)
-    # date_joined = models.DateField(auto_now_add=True)
-
-    # date_joined
-    # email_verified
-
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    email = models.EmailField(_("email address"), blank=True, unique=True)
+    username_validator = None
+    username = None
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        swappable = "AUTH_USER_MODEL"
+
+    def get_username(self):
+        """Return the email for this User."""
+        return getattr(self, self.EMAIL_FIELD)
+
+    def clean(self):
+        setattr(self, self.EMAIL_FIELD, self.normalize_username(self.get_username()))
+
+    @classmethod
+    def normalize_username(cls, email):
+        return unicodedata.normalize("NFKC", email) if isinstance(email, str) else email
+
+    def __str__(self):
+        return f"{self.email}"
