774fd8fd06fae7a08a6992add3e5f8049c5c59fc
Krzychu <czernicki.krz@gmail.com>
2023-11-16 14:28:49 +0100

    feat(dashboards/models.py): create Address model

--- EDMS/dashboards/models.py
+++ EDMS/dashboards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    street_name = models.CharField(max_length=100)
+    street_number = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street_name} {self.street_number}\n{self.postcode} {self.city}\n{self.country}"
+
+    class Meta:
+        verbose_name_plural = "Addresses"
