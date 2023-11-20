04e2f98ece1f7614885abda50901c196d62892aa
Krzychu <czernicki.krz@gmail.com>
2023-11-20 17:26:50 +0100

    feat(dashboards/admin.py): create CustomAddressAdmin and CustomCompanyAdmin

--- EDMS/dashboards/admin.py
+++ EDMS/dashboards/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address, Company
+
+
+class CustomAddressAdmin(admin.ModelAdmin):
+    list_display = ("street_name", "street_number", "city", "postcode", "country")
+
+
+class CustomCompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "KRS_id", "REGON_id", "NIP_id")
+
+
+admin.site.register(Address, CustomAddressAdmin)
+admin.site.register(Company, CustomCompanyAdmin)
