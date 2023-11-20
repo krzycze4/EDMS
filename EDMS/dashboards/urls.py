f5dd48e54948b4f7c2dff7d6c8c53bd1c7628466
Krzychu <czernicki.krz@gmail.com>
2023-11-20 16:30:09 +0100

    feat(dashboards/urls.py): create endpoints - '', 'company/find/', 'company/create/', 'company/create/done/'

--- EDMS/dashboards/urls.py
+++ EDMS/dashboards/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
 
-from .views import test_view
+from .views import (
+    CreateCompanyDoneView,
+    CreateCompanyView,
+    DashboardView,
+    FindCompanyView,
+)
 
-urlpatterns = [path("", test_view, name="dashboard")]
+urlpatterns = [
+    path("", DashboardView.as_view(), name="dashboard"),
+    path("company/find/", FindCompanyView.as_view(), name="find_company"),
+    path("company/create/", CreateCompanyView.as_view(), name="create_company"),
+    path(
+        "company/create/done",
+        CreateCompanyDoneView.as_view(),
+        name="create_company_done",
+    ),
+]
