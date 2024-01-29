24cf93d290776c4fb29873295c74a65f312e3106
Krzychu <czernicki.krz@gmail.com>
2024-01-29 08:19:00 +0100

    feat(EDMS/orders/forms.py): add new feature - add company cost order

--- EDMS/orders/forms.py
+++ EDMS/orders/forms.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from typing import Any, Dict
 
-from companies.models import Company
 from django import forms
 from invoices.models import Invoice
 
@@ -30,10 +29,6 @@ class OrderCreateForm(forms.ModelForm):
                 attrs={"class": "form-control", "type": "date"}
             ),
         }
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.fields["company"].queryset = Company.objects.filter(is_mine=False)
 
     def clean(self) -> Dict[str, Any]:
         cleaned_data = super().clean()
@@ -85,11 +80,8 @@ class OrderUpdateForm(forms.ModelForm):
 class OrderManageInvoicesForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["income_invoices", "cost_invoices"]
+        fields = ["cost_invoices"]  # "income_invoices",
         widgets = {
-            "income_invoices": forms.SelectMultiple(
-                attrs={"class": "form-control  js-example-basic-multiple", "size": 3}
-            ),
             "cost_invoices": forms.SelectMultiple(
                 attrs={"class": "form-control js-example-basic-multiple", "size": 3}
             ),
@@ -97,12 +89,45 @@ class OrderManageInvoicesForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["income_invoices"].queryset = Invoice.objects.filter(
-            seller__is_mine=True, buyer=self.instance.company
-        )
+        if not self.instance.company.is_mine:
+            self.fields.append("income_invoices")
+            self.fields["income_invoices"].widget = forms.SelectMultiple(
+                attrs={"class": "form-control  js-example-basic-multiple", "size": 3}
+            )
+            self.fields["income_invoices"].queryset = Invoice.objects.filter(
+                seller__is_mine=True, buyer=self.instance.company
+            )
         self.fields["cost_invoices"].queryset = Invoice.objects.filter(
             buyer__is_mine=True
         )
+
+    def aggregate_linked_invoices(self):
+        family_income_invoices = []
+        chosen_income_invoices = list(self.cleaned_data["income_invoices"])
+        family_income_invoices.extend(chosen_income_invoices)
+        for income_invoice in family_income_invoices:
+            if (
+                income_invoice.linked_invoice
+                and income_invoice.linked_invoice not in family_income_invoices
+            ):
+                family_income_invoices.append(income_invoice.linked_invoice)
+            children_income_invoices = list(
+                Invoice.objects.filter(linked_invoice=income_invoice)
+            )
+            if children_income_invoices:
+                for children_income_invoice in children_income_invoices:
+                    if children_income_invoice not in family_income_invoices:
+                        family_income_invoices.append(children_income_invoice)
+        return family_income_invoices
+
+    def save(self, commit=True):
+        order = super().save(commit=commit)
+        family_income_invoices = self.aggregate_linked_invoices()
+        income_invoices = Invoice.objects.filter(
+            pk__in=[invoice.pk for invoice in family_income_invoices]
+        )
+        order.income_invoices.set(income_invoices)
+        return order
 
 
 class ProtocolCreateForm(forms.ModelForm):
