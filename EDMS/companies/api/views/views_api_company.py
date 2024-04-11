14168f22509d9ff2e834b7a318cd99ce11a7d29a
Krzychu <czernicki.krz@gmail.com>
2024-04-11 14:51:19 +0200

    feat(companies): add next api authentication

--- EDMS/companies/api/views/views_api_company.py
+++ EDMS/companies/api/views/views_api_company.py
@@ -1,8 +1,12 @@
 from companies.api.serializers import CompanySerializer
 from companies.models import Company
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 
 class CompanyModelViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
--- EDMS/companies/api/views/views_api_contact.py
+++ EDMS/companies/api/views/views_api_contact.py
@@ -1,8 +1,12 @@
 from companies.api.serializers import ContactSerializer
 from companies.models import Contact
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 
 class ContactModelViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
