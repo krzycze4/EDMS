5180c7cd029fd1260108e4e2a0c9a5054c7d7bf7
Krzychu <czernicki.krz@gmail.com>
2024-05-16 13:59:33 +0200

    style: change div class for field Company.is_mine

--- EDMS/companies/forms/forms_company_and_address.py
+++ EDMS/companies/forms/forms_company_and_address.py
@@ -84,7 +84,10 @@ class UpdateCompanyIdentifiersForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
+            if field_name == "is_mine":
+                field.widget.attrs["class"] = "form-check-input ml-2"
+            else:
+                field.widget.attrs["class"] = "form-control"
 
 
 class UpdateAddressForm(forms.ModelForm):
