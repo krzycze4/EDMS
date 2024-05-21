06feac00bb99593958048ef9edcad7e7648a8372
Krzychu <czernicki.krz@gmail.com>
2024-05-21 13:06:32 +0200

    test(contracts/tests/test_factories.py): create tests to ContractFactory

---
+++ EDMS/contracts/tests/test_factories.py
@@ -0,0 +1,13 @@
+from contracts.factories import ContractFactory
+from contracts.models import Contract
+from django.test import TestCase
+
+
+class TestContractFactory(TestCase):
+    def test_create_correct_contract(self):
+        ContractFactory.create()
+        self.assertEqual(Contract.objects.count(), 1)
+
+    def test_create_correct_contract_bulk(self):
+        ContractFactory.create_batch(10)
+        self.assertEqual(Contract.objects.count(), 10)
