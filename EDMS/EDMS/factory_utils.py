8dd09c2069627adbf0a7898a2cab644ccf875d83
Krzychu <czernicki.krz@gmail.com>
2024-06-03 17:01:50 +0200

    fix(EDMS/EDMS/factory_utils.py): change module random to module secrets

--- EDMS/EDMS/factory_utils.py
+++ EDMS/EDMS/factory_utils.py
@@ -1,8 +1,8 @@
-import random
+import secrets
 from datetime import datetime, timedelta
 
 
 def create_string_format_valid_date(first_date: str) -> str:
     first_date = datetime.strptime(first_date, "%Y-%m-%d").date()
-    second_date = first_date + timedelta(days=random.randint(0, 366))
+    second_date = first_date + timedelta(days=secrets.randbelow(366))
     return second_date.strftime("%Y-%m-%d")
