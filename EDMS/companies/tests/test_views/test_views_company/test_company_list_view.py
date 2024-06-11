2482db8f7c28738c1fa1275327158632ed207db7
Krzychu <czernicki.krz@gmail.com>
2024-06-11 12:58:23 +0200

    style(companies/tests/test_views/test_views_company/test_company_list_view.py): remove comments and change name of the parent class

--- EDMS/companies/tests/test_views/test_views_company/test_company_list_view.py
+++ EDMS/companies/tests/test_views/test_views_company/test_company_list_view.py
@@ -13,7 +13,7 @@ from EDMS.group_utils import create_group_with_permissions
 User = get_user_model()
 
 
-class TestCaseCompanyCreateView(TestCase):
+class TestCaseCompanyListView(TestCase):
     @classmethod
     def setUpTestData(cls):
         group_names_with_permission_codenames = {
@@ -38,20 +38,6 @@ class TestCaseCompanyCreateView(TestCase):
 
         cls.template_name = "companies/companies/list_company.html"
 
-        # cls.company = CompanyFactory.build()
-        # cls.company_session_data = {
-        #     "name": company.name,
-        #     "krs": company.krs,
-        #     "regon": company.regon,
-        #     "nip": company.nip,
-        #     "street_name": company.address.street_name,
-        #     "street_number": company.address.street_number,
-        #     "city": company.address.city,
-        #     "postcode": company.address.postcode,
-        #     "country": company.address.country,
-        #     "shortcut": company.shortcut,
-        # }
-
     @classmethod
     def create_user_with_group(cls, group_name: str) -> User:
         group = get_object_or_404(Group, name=group_name)
@@ -63,14 +49,14 @@ class TestCaseCompanyCreateView(TestCase):
         self.company_list = CompanyFactory.create_batch(11)
 
 
-class TestCaseGetCompanyListViewUserNotAuthenticated(TestCaseCompanyCreateView):
+class TestCaseGetCompanyListViewUserNotAuthenticated(TestCaseCompanyListView):
     def test_get_user_not_authenticated(self):
         response = self.client.get(reverse_lazy("list-company"))
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, f"{reverse_lazy('login')}?next={reverse_lazy('list-company')}")
 
 
-class TestCaseGetCompanyListViewUserGroupAccountants(TestCaseCompanyCreateView):
+class TestCaseGetCompanyListViewUserGroupAccountants(TestCaseCompanyListView):
     def setUp(self):
         super().setUp()
         self.login = self.client.login(email=self.accountant.email, password=self.password)
@@ -102,7 +88,7 @@ class TestCaseGetCompanyListViewUserGroupAccountants(TestCaseCompanyCreateView):
         self.assertEqual(str(response.context["page_obj"]), "<Page 1 of 1>")
 
 
-class TestCaseGetCompanyListViewUserGroupCeos(TestCaseCompanyCreateView):
+class TestCaseGetCompanyListViewUserGroupCeos(TestCaseCompanyListView):
     def setUp(self):
         super().setUp()
         self.login = self.client.login(email=self.ceo.email, password=self.password)
@@ -134,7 +120,7 @@ class TestCaseGetCompanyListViewUserGroupCeos(TestCaseCompanyCreateView):
         self.assertEqual(str(response.context["page_obj"]), "<Page 1 of 1>")
 
 
-class TestCaseGetCompanyListViewUserGroupHrs(TestCaseCompanyCreateView):
+class TestCaseGetCompanyListViewUserGroupHrs(TestCaseCompanyListView):
     def setUp(self):
         super().setUp()
         self.login = self.client.login(email=self.hr.email, password=self.password)
@@ -166,7 +152,7 @@ class TestCaseGetCompanyListViewUserGroupHrs(TestCaseCompanyCreateView):
         self.assertEqual(str(response.context["page_obj"]), "<Page 1 of 1>")
 
 
-class TestCaseGetCompanyListViewUserGroupManagers(TestCaseCompanyCreateView):
+class TestCaseGetCompanyListViewUserGroupManagers(TestCaseCompanyListView):
     def setUp(self):
         super().setUp()
         self.login = self.client.login(email=self.manager.email, password=self.password)
