49c8c745ecb0d52e6b4018871cc18eb3e8724b7e
Krzychu <czernicki.krz@gmail.com>
2024-06-12 18:28:13 +0200

    test(EDMS/companies/tests/test_views/test_views_contact/test_contact_create_view.py): create tests to ContactCreateView

---
+++ EDMS/companies/tests/test_views/test_views_contact/test_contact_create_view.py
@@ -0,0 +1,171 @@
+from http import HTTPStatus
+
+from companies.factories import CompanyFactory, ContactFactory
+from companies.models import Contact
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.shortcuts import get_object_or_404
+from django.test import TestCase
+from django.urls import reverse_lazy
+from users.factories import UserFactory
+
+from EDMS.group_utils import create_group_with_permissions
+
+User = get_user_model()
+
+
+class TestCaseContactCreateView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        group_names_with_permission_codenames = {
+            "ceos": [
+                "add_contact",
+                "change_contact",
+                "delete_contact",
+                "view_contact",
+            ],
+            "accountants": [
+                "add_contact",
+                "change_contact",
+                "delete_contact",
+                "view_contact",
+            ],
+            "managers": [
+                "add_contact",
+                "change_contact",
+                "delete_contact",
+                "view_contact",
+            ],
+            "hrs": [
+                "add_contact",
+                "change_contact",
+                "delete_contact",
+                "view_contact",
+            ],
+        }
+        for (group_name, permission_codenames) in group_names_with_permission_codenames.items():
+            create_group_with_permissions(group_name=group_name, permission_codenames=permission_codenames)
+
+        cls.password = User.objects.make_random_password()
+        cls.accountant = cls.create_user_with_group(group_name="accountants")
+        cls.ceo = cls.create_user_with_group(group_name="ceos")
+        cls.hr = cls.create_user_with_group(group_name="hrs")
+        cls.manager = cls.create_user_with_group(group_name="managers")
+
+    @classmethod
+    def create_user_with_group(cls, group_name: str) -> User:
+        group = get_object_or_404(Group, name=group_name)
+        user = UserFactory(is_active=True, password=cls.password)
+        user.groups.add(group)
+        return user
+
+    def setUp(self) -> None:
+        self.company = CompanyFactory.create()
+        self.contact = ContactFactory.create(company=self.company)
+        self.contact_stub = ContactFactory.stub(company=self.company)
+        self.contact_data = {
+            "name": self.contact_stub.name,
+            "email": self.contact_stub.email,
+            "phone": self.contact_stub.phone,
+            "description": self.contact_stub.description,
+            "company": self.company.pk,
+        }
+        self.not_logged_user_url = (
+            f"{reverse_lazy('login')}?next={reverse_lazy('create-contact', kwargs={'pk': self.company.pk})}"
+        )
+
+
+class TestCaseContactCreateViewUserNotAuthenticated(TestCaseContactCreateView):
+    def test_get_method_when_user_not_authenticated(self):
+        response = self.client.get(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}))
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.not_logged_user_url)
+
+    def test_post_method_when_user_not_authenticated(self):
+        response = self.client.post(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}), data={})
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.not_logged_user_url)
+
+
+class TestCaseCompanyCreateViewUserGroupAccountants(TestCaseContactCreateView):
+    def setUp(self) -> None:
+        super().setUp()
+        self.login = self.client.login(email=self.accountant.email, password=self.password)
+
+    def test_get_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        response = self.client.get(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_post_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        contact_counter = Contact.objects.count()
+        response = self.client.post(
+            reverse_lazy("create-contact", kwargs={"pk": self.company.pk}), data=self.contact_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Contact.objects.count(), contact_counter + 1)
+        self.assertRedirects(response, reverse_lazy("detail-company", kwargs={"pk": self.company.pk}))
+
+
+class TestCaseCompanyCreateViewUserGroupCeos(TestCaseContactCreateView):
+    def setUp(self) -> None:
+        super().setUp()
+        self.login = self.client.login(email=self.ceo.email, password=self.password)
+
+    def test_get_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        response = self.client.get(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_post_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        contact_counter = Contact.objects.count()
+        response = self.client.post(
+            reverse_lazy("create-contact", kwargs={"pk": self.company.pk}), data=self.contact_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Contact.objects.count(), contact_counter + 1)
+        self.assertRedirects(response, reverse_lazy("detail-company", kwargs={"pk": self.company.pk}))
+
+
+class TestCaseCompanyCreateViewUserGroupHrs(TestCaseContactCreateView):
+    def setUp(self) -> None:
+        super().setUp()
+        self.login = self.client.login(email=self.hr.email, password=self.password)
+
+    def test_get_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        response = self.client.get(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_post_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        contact_counter = Contact.objects.count()
+        response = self.client.post(
+            reverse_lazy("create-contact", kwargs={"pk": self.company.pk}), data=self.contact_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Contact.objects.count(), contact_counter + 1)
+        self.assertRedirects(response, reverse_lazy("detail-company", kwargs={"pk": self.company.pk}))
+
+
+class TestCaseCompanyCreateViewUserGroupManagers(TestCaseContactCreateView):
+    def setUp(self) -> None:
+        super().setUp()
+        self.login = self.client.login(email=self.manager.email, password=self.password)
+
+    def test_get_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        response = self.client.get(reverse_lazy("create-contact", kwargs={"pk": self.company.pk}))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_post_method_when_user_group_is_accountants(self):
+        self.assertTrue(self.login)
+        contact_counter = Contact.objects.count()
+        response = self.client.post(
+            reverse_lazy("create-contact", kwargs={"pk": self.company.pk}), data=self.contact_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Contact.objects.count(), contact_counter + 1)
+        self.assertRedirects(response, reverse_lazy("detail-company", kwargs={"pk": self.company.pk}))
