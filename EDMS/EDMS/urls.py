96006452644c5a9da559fb4eebe5147c52fbf612
Krzychu <czernicki.krz@gmail.com>
2024-06-18 16:43:40 +0200

    fix(EDMS/urls.py): change append to +=

--- EDMS/EDMS/urls.py
+++ EDMS/EDMS/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns.append(path("silk/", include("silk.urls", namespace="silk")))
-    urlpatterns.append(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
-    urlpatterns.append(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
