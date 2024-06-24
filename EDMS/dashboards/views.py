48676fcf58425ce02fee5fa787fc228221db8af1
Krzychu <czernicki.krz@gmail.com>
2024-06-24 17:09:13 +0200

    style(dashboards/views.py): remove comment

--- EDMS/dashboards/views.py
+++ EDMS/dashboards/views.py
@@ -1,11 +1,7 @@
 # ruff: noqa: F401
 from dashboards.plots import render_plot_for_user_group
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import (
-    AccessMixin,
-    LoginRequiredMixin,
-    PermissionRequiredMixin,
-)
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseForbidden
 from django.views.generic import TemplateView
 
@@ -13,7 +9,6 @@ User = get_user_model()
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    # PermissionRequiredMixin,
     template_name = "dashboards/dashboard.html"
     permission_required = "dashboards.view_dashboard"
 
