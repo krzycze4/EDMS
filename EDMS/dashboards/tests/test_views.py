c4a2dc947644b894f5e053a04d2f63bc377dfd76
Krzychu <czernicki.krz@gmail.com>
2024-06-24 17:08:49 +0200

    test(dashboards/tests/): create tests for DashboardView

---
+++ EDMS/dashboards/tests/test_views.py
@@ -0,0 +1,46 @@
+from http import HTTPStatus
+
+from common_tests.EDMSTestCase import EDMSTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+
+User = get_user_model()
+
+
+class ContractDetailViewTests(EDMSTestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.not_auth_user_url = f"{reverse_lazy('login')}?next={reverse_lazy('dashboard')}"
+        cls.template_name = "dashboards/dashboard.html"
+
+    def test_redirect_if_user_is_not_authenticated(self):
+        response = self.client.get(reverse_lazy("dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.not_auth_user_url)
+
+    def test_access_for_accountants(self):
+        login = self.client.login(email=self.accountant.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(reverse_lazy("dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_access_for_ceos(self):
+        login = self.client.login(email=self.ceo.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(reverse_lazy("dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+
+    def test_access_for_hrs(self):
+        login = self.client.login(email=self.hr.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(reverse_lazy("dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_access_for_managers(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(reverse_lazy("dashboard"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
