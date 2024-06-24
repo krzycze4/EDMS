b05f78e9a567c9dd6838b66b75641adbd7a08517
Krzychu <czernicki.krz@gmail.com>
2024-06-24 17:07:29 +0200

    feat(dashboards/views.py): override get method to set permissions for particular groups

--- EDMS/dashboards/views.py
+++ EDMS/dashboards/views.py
@@ -1,15 +1,28 @@
 # ruff: noqa: F401
-
 from dashboards.plots import render_plot_for_user_group
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    AccessMixin,
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
+from django.http import HttpResponseForbidden
 from django.views.generic import TemplateView
 
 User = get_user_model()
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
+    # PermissionRequiredMixin,
     template_name = "dashboards/dashboard.html"
+    permission_required = "dashboards.view_dashboard"
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if user.groups.filter(name__in=["ceos", "managers"]).exists():
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
