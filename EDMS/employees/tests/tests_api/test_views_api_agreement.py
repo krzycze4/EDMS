c4bd873ff8944f22fcb13f1326c99c8a32ee38e3
Krzychu <czernicki.krz@gmail.com>
2024-07-23 22:21:28 +0200

    test(employees/tests/tests_api/test_views_api_agreement.py): create tests to AgreementModelViewSet

--- EDMS/employees/tests/tests_api/test_views_api_agreement.py
+++ EDMS/employees/tests/tests_api/test_views_api_agreement.py
@@ -5,7 +5,6 @@ from common_tests.EDMSTestCase import EDMSTestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from employees.factories.factories_agreement import AgreementFactory
-from employees.models.models_addendum import Addendum
 from employees.models.models_agreement import Agreement
 from rest_framework import serializers
 from rest_framework.test import APIClient
@@ -26,10 +25,12 @@ class AgreementApiTestCase(EDMSTestCase):
             "type": self.agreement.type,
             "salary_gross": self.agreement.salary_gross,
             "create_date": self.agreement.create_date,
+            "start_date": self.agreement.start_date,
             "end_date": self.agreement.end_date,
             "end_date_actual": self.agreement.end_date_actual,
-            "user": employee.pk,
+            "user": employee.id,
             "scan": self.agreement.scan,
+            "is_current": self.agreement.is_current,
         }
 
     def test_unauthenticated_user_cannot_view_agreement_list(self):
@@ -69,154 +70,154 @@ class AgreementApiTestCase(EDMSTestCase):
     def test_accountant_cannot_create_agreement(self):
         login = self.client.login(email=self.accountant.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
-        response = self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
+        count_agreement_before_response = Agreement.objects.count()
+        response = self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_accountant_can_update_addendum(self):
+    def test_accountant_cannot_update_agreement(self):
         login = self.client.login(email=self.accountant.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.put(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_accountant_can_delete_addendum(self):
+    def test_accountant_cannot_delete_agreement(self):
         login = self.client.login(email=self.accountant.email, password=self.password)
         self.assertTrue(login)
-        count_address_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.delete(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_address_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_ceo_can_view_addendum_list(self):
+    def test_ceo_can_view_agreement_list(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-list"))
+        response = self.client.get(reverse_lazy("agreement-list"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_ceo_can_view_addendum_detail(self):
+    def test_ceo_can_view_agreement_detail(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}))
+        response = self.client.get(reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_ceo_can_create_addendum(self):
+    def test_ceo_can_create_agreement(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
-        response = self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
+        count_agreement_before_response = Agreement.objects.count()
+        response = self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response + 1)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response + 1)
 
-    def test_ceo_can_update_addendum(self):
+    def test_ceo_can_update_agreement(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.put(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_ceo_can_delete_addendum(self):
+    def test_ceo_can_delete_agreement(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.delete(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NO_CONTENT)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response - 1)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response - 1)
 
-    def test_hr_can_view_addendum_list(self):
+    def test_hr_can_view_agreement_list(self):
         login = self.client.login(email=self.hr.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-list"))
+        response = self.client.get(reverse_lazy("agreement-list"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_hr_can_view_addendum_detail(self):
+    def test_hr_can_view_agreement_detail(self):
         login = self.client.login(email=self.hr.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}))
+        response = self.client.get(reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_hr_can_create_addendum(self):
+    def test_hr_can_create_agreement(self):
         login = self.client.login(email=self.hr.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
-        response = self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
+        count_agreement_before_response = Agreement.objects.count()
+        response = self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response + 1)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response + 1)
 
-    def test_hr_can_update_addendum(self):
+    def test_hr_can_update_agreement(self):
         login = self.client.login(email=self.hr.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.put(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_hr_can_delete_addendum(self):
+    def test_hr_can_delete_agreement(self):
         login = self.client.login(email=self.hr.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.delete(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NO_CONTENT)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response - 1)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response - 1)
 
-    def test_manager_can_view_addendum_list(self):
+    def test_manager_can_view_agreement_list(self):
         login = self.client.login(email=self.manager.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-list"))
+        response = self.client.get(reverse_lazy("agreement-list"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_manager_can_view_addendum_detail(self):
+    def test_manager_can_view_agreement_detail(self):
         login = self.client.login(email=self.manager.email, password=self.password)
         self.assertTrue(login)
-        response = self.client.get(reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}))
+        response = self.client.get(reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_manager_can_create_addendum(self):
+    def test_manager_cannot_create_agreement(self):
         login = self.client.login(email=self.manager.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
-        response = self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
+        count_agreement_before_response = Agreement.objects.count()
+        response = self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_manager_can_update_addendum(self):
+    def test_manager_cannot_update_agreement(self):
         login = self.client.login(email=self.manager.email, password=self.password)
         self.assertTrue(login)
-        count_addendum_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.put(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_addendum_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
-    def test_manager_can_delete_addendum(self):
+    def test_manager_cannot_delete_agreement(self):
         login = self.client.login(email=self.manager.email, password=self.password)
         self.assertTrue(login)
-        count_address_before_response = Addendum.objects.count()
+        count_agreement_before_response = Agreement.objects.count()
         response = self.client.delete(
-            reverse_lazy("addendum-detail", kwargs={"pk": self.addendum_list[0].id}), self.addendum_data
+            reverse_lazy("agreement-detail", kwargs={"pk": self.agreement_list[0].id}), self.agreement_data
         )
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
-        self.assertEqual(Addendum.objects.count(), count_address_before_response)
+        self.assertEqual(Agreement.objects.count(), count_agreement_before_response)
 
     def test_prevent_duplicate_addendum_creation(self):
         login = self.client.login(email=self.ceo.email, password=self.password)
         self.assertTrue(login)
-        self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
-        self.client.post(reverse_lazy("addendum-list"), self.addendum_data)
+        self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
+        self.client.post(reverse_lazy("agreement-list"), self.agreement_data)
         self.assertRaises(serializers.ValidationError)
