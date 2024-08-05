ca6be01147ad615a57c02b16c28fb4aff937dc80
Krzychu <czernicki.krz@gmail.com>
2024-08-05 02:10:32 +0200

    test(employees/tests/tests_views/tests_views_agreement/tests_delete_view_agreement.py): create tests to AgreementDeleteView

---
+++ EDMS/employees/tests/tests_views/tests_views_agreement/tests_delete_view_agreement.py
@@ -0,0 +1,92 @@
+from http import HTTPStatus
+
+from common_tests.EDMSTestCase import EDMSTestCase
+from django.urls import reverse_lazy
+from employees.factories.factories_agreement import AgreementFactory
+from employees.models.models_agreement import Agreement
+
+
+class AgreementDeleteViewTests(EDMSTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.agreement = AgreementFactory.create()
+        self.view_url = reverse_lazy("delete-agreement", kwargs={"pk": self.agreement.pk})
+        self.login_redirect_url = f"{reverse_lazy('login')}?next={self.view_url}"
+        self.template_name = "employees/agreements/agreement_delete.html"
+        self.success_url = reverse_lazy("detail-employee", kwargs={"pk": self.agreement.user.pk})
+
+    def test_redirect_to_login_page_when_not_authenticated_user_execute_get_method(self):
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.login_redirect_url)
+
+    def test_redirect_to_login_page_when_not_authenticated_user_execute_post_method(self):
+        response = self.client.post(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.login_redirect_url)
+
+    def test_deny_render_delete_view_when_logged_user_group_accountants_execute_get_method(self):
+        login = self.client.login(email=self.accountant.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_deny_delete_agreement_when_logged_user_group_accountants_execute_post_method(self):
+        login = self.client.login(email=self.accountant.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 1
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        response = self.client.post(self.view_url)
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_render_delete_view_when_logged_user_group_ceos_execute_get_method(self):
+        login = self.client.login(email=self.ceo.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+
+    def test_delete_agreement_when_logged_user_group_ceos_execute_post_method(self):
+        login = self.client.login(email=self.ceo.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 1
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        response = self.client.post(self.view_url)
+        expected_value = 0
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.success_url)
+
+    def test_deny_render_delete_view_when_logged_user_group_managers_execute_get_method(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_deny_delete_agreement_when_logged_user_group_managers_execute_post_method(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 1
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        response = self.client.post(self.view_url)
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_render_delete_view_when_logged_user_group_hrs_execute_get_method(self):
+        login = self.client.login(email=self.hr.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+
+    def test_delete_agreement_when_logged_user_group_hrs_execute_post_method(self):
+        login = self.client.login(email=self.hr.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 1
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        response = self.client.post(self.view_url)
+        expected_value = 0
+        self.assertEqual(Agreement.objects.count(), expected_value)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.success_url)
