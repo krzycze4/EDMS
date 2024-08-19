3f4a4ef7c0508a89750c3c95789643100a29862e
Krzychu <czernicki.krz@gmail.com>
2024-08-19 13:09:45 +0200

    feat(orders/factories.py): create ProtocolFactory

--- EDMS/orders/factories.py
+++ EDMS/orders/factories.py
@@ -4,10 +4,11 @@ from decimal import Decimal
 import factory
 from companies.factories import CompanyFactory
 from contracts.factories import ContractFactory
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils import timezone
 from factory import Sequence
 from factory.django import DjangoModelFactory
-from orders.models import Order
+from orders.models import Order, Protocol
 from users.factories import UserFactory
 
 
@@ -25,3 +26,17 @@ class OrderFactory(DjangoModelFactory):
     start_date = factory.LazyAttribute(lambda obj: obj.create_date - timedelta(days=1))
     end_date = factory.LazyAttribute(lambda obj: obj.create_date + timedelta(days=365))
     description = "XYZ"
+
+
+class ProtocolFactory(DjangoModelFactory):
+    class Meta:
+        model = Protocol
+
+    id = factory.Sequence(lambda n: n + 1)
+    name = factory.Sequence(lambda n: f"protocol_{n + 1}")
+    scan = factory.LazyAttribute(
+        lambda _: SimpleUploadedFile("the_file.pdf", b"file_content", content_type="application/pdf")
+    )
+    create_date = factory.LazyAttribute(lambda _: timezone.now().date())
+    user = factory.SubFactory(UserFactory)
+    order = factory.SubFactory(OrderFactory)
