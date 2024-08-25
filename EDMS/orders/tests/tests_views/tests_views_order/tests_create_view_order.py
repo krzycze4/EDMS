d83370d02713c53ecd573fd7a834af26b2ea6d62
Krzychu <czernicki.krz@gmail.com>
2024-08-25 19:27:36 +0200

    test(orders/tests/tests_views/tests_create_view_order.py): create tests to OrderCreateView

---
+++ EDMS/orders/tests/tests_views/tests_views_order/tests_create_view_order.py
@@ -0,0 +1,114 @@
+from http import HTTPStatus
+
+from common_tests.EDMSTestCase import EDMSTestCase
+from contracts.factories import ContractFactory
+from django.test import tag
+from django.urls import reverse_lazy
+from orders.factories import OrderFactory
+from orders.models import Order
+from users.factories import UserFactory
+
+
+class OrderCreateViewTests(EDMSTestCase):
+    def setUp(self) -> None:
+        user = UserFactory.create()
+        contract = ContractFactory.create()
+        self.order = OrderFactory.build(
+            company=contract.company, user=user, contract=contract, start_date=contract.start_date
+        )
+        self.view_url = reverse_lazy("create-order")
+        self.redirect_login_url = f"{reverse_lazy('login')}?next={self.view_url}"
+        self.template_name = "orders/orders/create_order.html"
+        self.order_data = {
+            "payment": self.order.payment,
+            "company": self.order.company.pk,
+            "start_date": self.order.start_date,
+            "create_date": self.order.create_date,
+            "end_date": self.order.end_date,
+            "contract": self.order.contract.pk,
+            "description": self.order.description,
+        }
+
+    def test_redirect_to_login_page_when_not_authenticated_user_execute_get_method(self):
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.redirect_login_url)
+
+    def test_redirect_to_login_page_when_not_authenticated_user_execute_post_method(self):
+        response = self.client.post(self.view_url, data=self.order_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, self.redirect_login_url)
+
+    def test_deny_render_create_view_when_logged_user_group_accountants_execute_get_method(self):
+        login = self.client.login(email=self.accountant.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_deny_create_object_when_logged_user_group_accountants_execute_post_method(self):
+        login = self.client.login(email=self.accountant.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 0
+        self.assertEqual(Order.objects.count(), expected_value)
+        response = self.client.post(self.view_url, data=self.order_data)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(Order.objects.count(), expected_value)
+
+    def test_render_create_view_when_logged_user_group_ceos_execute_get_method(self):
+        login = self.client.login(email=self.ceo.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+
+    def test_create_object_when_logged_user_group_ceos_execute_post_method(self):
+        login = self.client.login(email=self.ceo.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 0
+        self.assertEqual(Order.objects.count(), expected_value)
+        response = self.client.post(self.view_url, data=self.order_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        expected_value += 1
+        self.assertEqual(Order.objects.count(), expected_value)
+        success_url = reverse_lazy("detail-order", kwargs={"pk": Order.objects.last().pk})
+        self.assertRedirects(response, success_url)
+
+    def test_deny_render_create_view_when_logged_user_group_hrs_execute_get_method(self):
+        login = self.client.login(email=self.hr.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_deny_create_object_when_logged_user_group_hrs_execute_post_method(self):
+        login = self.client.login(email=self.hr.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 0
+        self.assertEqual(Order.objects.count(), expected_value)
+        response = self.client.post(self.view_url, data=self.order_data)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(Order.objects.count(), expected_value)
+
+    def test_render_create_view_when_logged_user_group_managers_execute_get_method(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+
+    @tag("x")
+    def test_create_object_when_logged_user_group_managers_execute_post_method(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        expected_value = 0
+        self.assertEqual(Order.objects.count(), expected_value)
+        response = self.client.post(self.view_url, data=self.order_data)
+        if response.context and "form" in response.context:
+            form = response.context["form"]
+            if not form.is_valid():
+                # Print all errors related to fields
+                print("Form errors (by field):", form.errors)
+                # Print non-field errors (general errors not tied to a specific field)
+                print("Form non-field errors:", form.non_field_errors())
+        expected_value += 1
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Order.objects.count(), expected_value)
