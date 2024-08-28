af151fd08b1dda9bbd4965684fae87f535487e98
Krzychu <czernicki.krz@gmail.com>
2024-08-28 17:46:25 +0200

    test(orders/tests/tests_views/tests_views_order/tests_detail_view_order.py): add test_count_invoices_sum_when_invoices_duplicate_and_correcting_exist

--- EDMS/orders/tests/tests_views/tests_views_order/tests_detail_view_order.py
+++ EDMS/orders/tests/tests_views/tests_views_order/tests_detail_view_order.py
@@ -1,8 +1,12 @@
+from decimal import Decimal
 from http import HTTPStatus
 
 from common_tests.EDMSTestCase import EDMSTestCase
+from companies.factories import CompanyFactory
 from contracts.factories import ContractFactory
 from django.urls import reverse_lazy
+from invoices.factories import InvoiceFactory
+from invoices.models import Invoice
 from orders.factories import OrderFactory
 
 
@@ -45,3 +49,35 @@ class OrderDetailViewTests(EDMSTestCase):
         response = self.client.get(self.view_url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, self.template_name)
+
+    def test_count_invoices_sum_when_invoices_original_and_correcting_exist(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        seller = CompanyFactory.create()
+        buyer = CompanyFactory.create(is_mine=True)
+        original = InvoiceFactory.create(seller=seller, buyer=buyer)
+        correcting = InvoiceFactory.create(
+            seller=seller, buyer=buyer, linked_invoice=original, net_price=Decimal(5999), type=Invoice.CORRECTING
+        )
+        self.order.cost_invoice.add(original)
+        self.order.cost_invoice.add(correcting)
+        self.order.save()
+        response = self.client.get(self.view_url)
+        expected_value = Decimal(5999)
+        self.assertEqual(response.context["cost_invoices_net_price_sum"], expected_value)
+
+    def test_count_invoices_sum_when_invoices_duplicate_and_correcting_exist(self):
+        login = self.client.login(email=self.manager.email, password=self.password)
+        self.assertTrue(login)
+        seller = CompanyFactory.create()
+        buyer = CompanyFactory.create(is_mine=True)
+        duplicate = InvoiceFactory.create(seller=seller, buyer=buyer, type=Invoice.DUPLICATE)
+        correcting = InvoiceFactory.create(
+            seller=seller, buyer=buyer, linked_invoice=duplicate, net_price=Decimal(5999), type=Invoice.CORRECTING
+        )
+        self.order.cost_invoice.add(duplicate)
+        self.order.cost_invoice.add(correcting)
+        self.order.save()
+        response = self.client.get(self.view_url)
+        expected_value = Decimal(5999)
+        self.assertEqual(response.context["cost_invoices_net_price_sum"], expected_value)
