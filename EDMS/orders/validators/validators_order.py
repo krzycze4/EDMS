71ce73933101608b93c201e3bb537824643a1026
Krzychu <czernicki.krz@gmail.com>
2024-08-29 20:45:06 +0200

    docs(orders/validators/validators_order.py): create docstrings

--- EDMS/orders/validators/validators_order.py
+++ EDMS/orders/validators/validators_order.py
@@ -11,14 +11,36 @@ from orders.models import Order
 
 
 class OrderCreateValidator:
+    """
+    Handles validation of OrderCreateForm data.
+    """
+
     @classmethod
     def all_validators(cls) -> List[callable]:
+        """
+        Checks if the end date is after the start date.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the end date is before the start date.
+        """
         return [func for _, func in inspect.getmembers(cls, predicate=inspect.isfunction)]
 
     @staticmethod
     def validate_end_date_after_start_date(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Checks if an order with the same data already exists.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If an order with the same payment, company, start date, and end date exists.
+        """
         start_date: date = cleaned_data["start_date"]
         end_date: date = cleaned_data["end_date"]
         if start_date > end_date:
@@ -26,6 +48,15 @@ class OrderCreateValidator:
 
     @staticmethod
     def validate_no_repetition(cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]) -> None:
+        """
+        Checks if the create date is not in the future.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the create date is in the future.
+        """
         start_date: date = cleaned_data["start_date"]
         end_date: date = cleaned_data["end_date"]
         payment: Decimal = cleaned_data["payment"]
@@ -41,6 +72,15 @@ class OrderCreateValidator:
     def validate_no_future_create_date(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Checks if the start date of the order is within the contract period.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the start date of the order is not within the contract period.
+        """
         create_date = cleaned_data["create_date"]
         if timezone.now().date() < create_date:
             raise ValidationError({"create_date": "The create date can't be future end_date."})
@@ -49,6 +89,15 @@ class OrderCreateValidator:
     def validate_start_date_in_contract_period(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Checks if the company in the order is the same as the company in the contract.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the company in the order and the company in the contract are different.
+        """
         start_date_order: date = cleaned_data["start_date"]
         start_date_contract: date = cleaned_data["contract"].start_date
         end_date_contract: date = cleaned_data["contract"].end_date
@@ -60,6 +109,12 @@ class OrderCreateValidator:
     def validate_same_company_in_order_and_contract(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Gets a list of all validator methods in this class.
+
+        Returns:
+            List[callable]: A list of all validator functions.
+        """
         company_in_order: Company = cleaned_data["company"]
         company_in_contract: Company = cleaned_data["contract"].company
         if not company_in_order == company_in_contract:
@@ -67,14 +122,33 @@ class OrderCreateValidator:
 
 
 class OrderUpdateValidator:
+    """
+    Handles validation of OrderUpdateForm data.
+    """
+
     @classmethod
     def all_validators(cls) -> List[callable]:
+        """
+        Gets a list of all validator methods in this class.
+
+        Returns:
+            List[callable]: A list of all validator functions.
+        """
         return [func for _, func in inspect.getmembers(cls, predicate=inspect.isfunction)]
 
     @staticmethod
     def validate_start_date_in_contract_period(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Checks if the start date of the order is within the contract period.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the start date of the order is not within the contract period.
+        """
         start_date_order: date = cleaned_data["start_date"]
         start_date_contract: date = cleaned_data["contract"].start_date
         end_date_contract: date = cleaned_data["contract"].end_date
@@ -86,6 +160,15 @@ class OrderUpdateValidator:
     def validate_end_date_after_start_date(
         cleaned_data: Dict[str, Union[Decimal | Company | str | date | Contract]]
     ) -> None:
+        """
+        Checks if the end date is after the start date.
+
+        Args:
+            cleaned_data (Dict[str, Union[Decimal | Company | str | date | Contract]]): The cleaned data from the form.
+
+        Raises:
+            ValidationError: If the end date is before the start date.
+        """
         start_date: date = cleaned_data["start_date"]
         end_date: date = cleaned_data["end_date"]
         if start_date > end_date:
