c735ecb21e742659eb9907d561c6f380c368e062
Krzychu <czernicki.krz@gmail.com>
2024-08-29 13:43:54 +0200

    docs(EDMS/group_utils.py): create docstrings

--- EDMS/EDMS/group_utils.py
+++ EDMS/EDMS/group_utils.py
@@ -3,6 +3,16 @@ from typing import List
 
 
 def create_group_with_permissions(group_name: str, permission_codenames: List[str]) -> "Group":
+    """
+    Create a group with the specified permissions.
+
+    Args:
+        group_name (str): The name of the group to create or update.
+        permission_codenames (List[str]): A list of permission codenames to assign to the group.
+
+    Returns:
+        Group: The created or updated group with the assigned permissions.
+    """
     from django.contrib.auth.models import Group, Permission
 
     permissions = Permission.objects.filter(codename__in=permission_codenames)
