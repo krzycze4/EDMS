d366450fb4145832ee6a9f20fb872fea02652e12
Krzychu <czernicki.krz@gmail.com>
2024-08-29 20:34:34 +0200

    docs(invoices/validators.py): create docstrings

--- EDMS/invoices/validators.py
+++ EDMS/invoices/validators.py
@@ -12,14 +12,33 @@ from invoices.models import Invoice
 
 
 class InvoiceValidator:
+    """
+    Handles validation of InvoiceForm data.
+    """
+
     @classmethod
     def all_validators(cls) -> List[callable]:
+        """
+        Gets all the validation methods in this class.
+
+        Returns:
+            List[callable]: A list of all the validator methods in the class.
+        """
         return [func for _, func in inspect.getmembers(cls, predicate=inspect.isfunction)]
 
     @staticmethod
     def validate_seller_different_than_buyer(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if the seller and buyer are different.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the seller is the same as the buyer.
+        """
         seller: Company = cleaned_data["seller"]
         buyer: Company = cleaned_data["buyer"]
 
@@ -30,6 +49,15 @@ class InvoiceValidator:
     def validate_max_vat(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]]
     ) -> None:
+        """
+        Checks if the VAT is within the allowed maximum.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the VAT is greater than 23% of the net price.
+        """
         vat: Decimal = cleaned_data["vat"]
         net_price: Decimal = cleaned_data["net_price"]
 
@@ -43,6 +71,15 @@ class InvoiceValidator:
     def validate_net_price_plus_vat_equal_gross(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if the net price plus VAT equals the gross amount.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the net price plus VAT does not equal the gross amount.
+        """
         net_price: Decimal = cleaned_data["net_price"]
         vat: Decimal = cleaned_data["vat"]
         gross: Decimal = cleaned_data["gross"]
@@ -54,6 +91,15 @@ class InvoiceValidator:
     def validate_no_future_create_date(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if the invoice's create date is not in the future.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the create date is in the future.
+        """
         today: date = timezone.now().date()
         create_date: date = cleaned_data["create_date"]
 
@@ -64,6 +110,16 @@ class InvoiceValidator:
     def validate_no_payment_date_before_create_date(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if the payment date is not before the create date, except for certain invoice types.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the payment date is before the create date for invoices that are not duplicates or
+            correcting invoices.
+        """
         if cleaned_data["type"] not in [Invoice.DUPLICATE, Invoice.CORRECTING]:
             payment_date: date = cleaned_data["payment_date"]
             create_date: date = cleaned_data["create_date"]
@@ -75,6 +131,15 @@ class InvoiceValidator:
     def validate_seller_or_buyer_must_be_my_company(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if either the seller or the buyer is the user's company.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If neither the seller nor the buyer is the user's company.
+        """
         buyer: Company = cleaned_data["buyer"]
         seller: Company = cleaned_data["seller"]
         if not buyer.is_mine and not seller.is_mine:
@@ -89,6 +154,15 @@ class InvoiceValidator:
     def validate_original_invoice_not_linked_to_other_invoice(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if an original invoice is linked to another invoice.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If an original invoice is linked to another invoice.
+        """
         invoice_type: str = cleaned_data["type"]
         linked_invoice: Invoice = cleaned_data["linked_invoice"]
         if linked_invoice and invoice_type == Invoice.ORIGINAL:
@@ -98,6 +172,15 @@ class InvoiceValidator:
     def validate_proforma_and_duplicate_same_data_as_original(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if proforma or duplicate invoices have the same data as the original.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the data in proforma or duplicate invoices is different from the original.
+        """
         linked_invoice: Invoice = cleaned_data["linked_invoice"]
         if linked_invoice:
             invoice_type: str = cleaned_data["type"]
@@ -116,6 +199,15 @@ class InvoiceValidator:
     def validate_correcting_invoice_linked_with_original_or_duplicate(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]],
     ) -> None:
+        """
+        Checks if a correcting invoice is linked with an original or duplicate invoice.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If a correcting invoice is linked to a proforma invoice instead of an original or duplicate.
+        """
         invoice_type: str = cleaned_data["type"]
         linked_invoice: Invoice = cleaned_data["linked_invoice"]
         if invoice_type == Invoice.CORRECTING and linked_invoice.type == Invoice.PROFORMA:
@@ -127,6 +219,15 @@ class InvoiceValidator:
     def validate_file_extension(
         cleaned_data: Dict[str, Union[str | Decimal | date | Company | UploadedFile | Invoice | bool]]
     ) -> None:
+        """
+        Checks if the file extension of the uploaded scan is valid.
+
+        Args:
+            cleaned_data (Dict): The cleaned data dictionary containing the invoice details.
+
+        Raises:
+            ValidationError: If the file extension is not in the list of allowed extensions.
+        """
         scan: UploadedFile = cleaned_data["scan"]
         extension: str = os.path.splitext(scan.name)[1]
         valid_extensions = [
