194b1dd04b024d94576c6ab50deeb99e7d45ad8d
Krzychu <czernicki.krz@gmail.com>
2024-08-29 17:51:29 +0200

    docs(invoices/apps.py): creaete docstrings

--- EDMS/invoices/apps.py
+++ EDMS/invoices/apps.py
@@ -6,6 +6,22 @@ from EDMS.group_utils import create_group_with_permissions
 
 
 def define_groups(sender, **kwargs):
+    """
+    Creates user groups and sets group's model permissions from employees app.
+
+    Groups:
+    - CEOs,
+    - accountants,
+    - HRs (exists in project, but doesn't have Invoice permission at all),
+    - managers.
+
+    Args:
+        sender (Any): The sender of the signal, typically not used in this function.
+        **kwargs: Additional keyword arguments passed to the function.
+
+    Returns:
+        None: This function does not return anything. It creates groups and sets permissions.
+    """
     group_names_with_permission_codenames = {
         "ceos": ["add_invoice", "change_invoice", "delete_invoice", "view_invoice"],
         "accountants": [
