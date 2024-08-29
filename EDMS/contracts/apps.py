1ad1173e192a015b6a8f2ff54ad2add1310bc3fb
Krzychu <czernicki.krz@gmail.com>
2024-08-29 12:07:52 +0200

    docs(contracts/apps.py): create docstrings

--- EDMS/contracts/apps.py
+++ EDMS/contracts/apps.py
@@ -4,6 +4,22 @@ from EDMS.group_utils import create_group_with_permissions
 
 
 def define_groups(sender, **kwargs):
+    """
+    Creates user groups and sets group's model permissions from contracts app.
+
+    Groups:
+    - CEOs,
+    - accountants,
+    - HRs (exists in project, but doesn't have Contract permission at all),
+    - managers.
+
+    Args:
+        sender (Any): The sender of the signal, typically not used in this function.
+        **kwargs: Additional keyword arguments passed to the function.
+
+    Returns:
+        None: This function does not return anything. It creates groups and sets permissions.
+    """
     group_names_with_permission_codenames = {
         "ceos": ["add_contract", "change_contract", "delete_contract", "view_contract"],
         "accountants": ["view_contract"],
