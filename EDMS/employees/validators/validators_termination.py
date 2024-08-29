e5da7b51c10f9d4ead229bed2bebd17d0b18fae9
Krzychu <czernicki.krz@gmail.com>
2024-08-29 15:33:57 +0200

    docs(employees/validators/validators_termination.py): create docstrings

--- EDMS/employees/validators/validators_termination.py
+++ EDMS/employees/validators/validators_termination.py
@@ -8,12 +8,31 @@ from employees.models.models_agreement import Agreement
 
 
 class TerminationValidator:
+    """
+    Validator checks TerminationFrom data.
+    """
+
     @classmethod
     def all_validators(cls) -> List[callable]:
+        """
+        Gets a list of all validator methods in the class.
+
+        Returns:
+            List[callable]: A list of all functions in the class that are used for validation.
+        """
         return [func for _, func in inspect.getmembers(cls, predicate=inspect.isfunction)]
 
     @staticmethod
     def validate_termination_dates(cleaned_data: Dict[str, Union[str | date | Agreement | UploadedFile]]) -> None:
+        """
+        Checks if the termination dates are valid.
+
+        Args:
+            cleaned_data (Dict[str, Union[str | date | Agreement | UploadedFile]]): A dictionary containing termination data to be validated.
+
+        Raises:
+            ValidationError: If any of the dates are invalid based on the agreement dates.
+        """
         agreement: Agreement = cleaned_data["agreement"]
         create_date: date = cleaned_data["create_date"]
         end_date: date = cleaned_data["end_date"]
