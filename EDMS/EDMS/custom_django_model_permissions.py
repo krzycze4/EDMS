c18597c97b641376c7a0f9d22faf2af08b6ea4a3
Krzychu <czernicki.krz@gmail.com>
2024-08-29 13:39:10 +0200

    docs(EDMS/custom_django_model_permissions.py): create docstrings

--- EDMS/EDMS/custom_django_model_permissions.py
+++ EDMS/EDMS/custom_django_model_permissions.py
@@ -2,6 +2,19 @@ from rest_framework.permissions import DjangoModelPermissions
 
 
 class CustomDjangoModelPermissions(DjangoModelPermissions):
+    """
+    Custom permissions for Django models based on HTTP methods.
+
+    This class maps HTTP methods to model permissions. For example:
+    - GET, OPTIONS, HEAD require view permissions.
+    - POST requires add permission.
+    - PUT and PATCH require change permissions.
+    - DELETE requires delete permission.
+
+    Attributes:
+        perms_map (dict): A dictionary mapping HTTP methods to permissions.
+    """
+
     perms_map = {
         "GET": ["%(app_label)s.view_%(model_name)s"],
         "OPTIONS": ["%(app_label)s.view_%(model_name)s"],
