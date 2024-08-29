aab321527ee20e9866bc89861c2642ae9eccb142
Krzychu <czernicki.krz@gmail.com>
2024-08-29 22:12:22 +0200

    docs(users/tokens.py): create docstrings

--- EDMS/users/tokens.py
+++ EDMS/users/tokens.py
@@ -5,7 +5,25 @@ User = get_user_model()
 
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    """
+    This class generates tokens for user account activation.
+
+    It extends PasswordResetTokenGenerator to create tokens that can be used
+    to activate user accounts. Tokens are based on the user's primary key,
+    the current timestamp, and the user's active status.
+    """
+
     def _make_hash_value(self, user: User, timestamp: int) -> str:
+        """
+        Creates a hash value for the token.
+
+        Args:
+            user (User): The user for whom the token is being generated.
+            timestamp (int): The current time in seconds since the epoch.
+
+        Returns:
+            str: A string used to create the token hash.
+        """
         return str(user.pk) + str(timestamp) + str(user.is_active)
 
 
