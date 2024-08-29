b0a7b2e85a43b22d91d6721837a0cd5c292bd4e1
Krzychu <czernicki.krz@gmail.com>
2024-08-29 22:12:52 +0200

    refactor(invoices/factories.py): rafactor date fields

--- EDMS/invoices/factories.py
+++ EDMS/invoices/factories.py
@@ -18,9 +18,9 @@ class InvoiceFactory(DjangoModelFactory):
     net_price = Decimal(6000)
     vat = (net_price * Decimal(0.23)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
     gross = (net_price + vat).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-    create_date = factory.LazyAttribute(lambda _: timezone.now().date())
-    service_date = factory.LazyAttribute(lambda obj: obj.create_date - timezone.timedelta(days=1))
-    payment_date = factory.LazyAttribute(lambda obj: obj.create_date + timezone.timedelta(days=30))
+    create_date = timezone.now().date()
+    service_date = create_date - timezone.timedelta(days=1)
+    payment_date = create_date + timezone.timedelta(days=30)
     type = Invoice.ORIGINAL
     linked_invoice = None
     scan = factory.LazyAttribute(
