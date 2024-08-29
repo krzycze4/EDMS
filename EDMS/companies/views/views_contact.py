b30d708e1b77180a98cd0bf4c31da0e179f41a8f
Krzychu <czernicki.krz@gmail.com>
2024-08-29 11:57:10 +0200

    refactor(companies/views/views_contact.py): use get_object_or_404 shortcut

--- EDMS/companies/views/views_contact.py
+++ EDMS/companies/views/views_contact.py
@@ -1,6 +1,7 @@
 from companies.forms.forms_contact import CreateContactForm, UpdateContactForm
 from companies.models import Company, Contact
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, UpdateView
 
@@ -19,8 +20,7 @@ class ContactCreateView(PermissionRequiredMixin, CreateView, LoginRequiredMixin)
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data()
         company_pk = self.kwargs["pk"]
-        company = Company.objects.get(id=company_pk)
-        context["company"] = company
+        context["company"] = get_object_or_404(Company, id=company_pk)
         return context
 
     def get_success_url(self):
@@ -34,10 +34,8 @@ class ContactUpdateView(PermissionRequiredMixin, UpdateView, LoginRequiredMixin)
     template_name = "companies/contacts/update_contact.html"
 
     def get_object(self, **kwargs):
-        company_pk = self.kwargs["company_pk"]
         contact_pk = self.kwargs["contact_pk"]
-        contact = Contact.objects.get(id=contact_pk, company_id=company_pk)
-        return contact
+        return get_object_or_404(Contact, id=contact_pk)
 
     def get_success_url(self):
         return reverse_lazy("detail-company", kwargs={"pk": self.kwargs["company_pk"]})
@@ -49,10 +47,8 @@ class ContactDeleteView(PermissionRequiredMixin, DeleteView, LoginRequiredMixin)
     template_name = "companies/contacts/delete_contact.html"
 
     def get_object(self, **kwargs):
-        company_pk = self.kwargs["company_pk"]
         contact_pk = self.kwargs["contact_pk"]
-        contact = Contact.objects.get(id=contact_pk, company_id=company_pk)
-        return contact
+        return get_object_or_404(Contact, id=contact_pk)
 
     def get_success_url(self):
         return reverse_lazy("detail-company", kwargs={"pk": self.kwargs["company_pk"]})
